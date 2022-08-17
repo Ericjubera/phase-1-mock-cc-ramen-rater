e1eb42d49be751c26ed05841f9adfab11f4d25c8
eric jubera <ericjubera@gmail.com>
2022-08-17 10:58:39 -0500

    yes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+const apiKey="http://localhost:3000/ramens"
+
+/
+
+
+function RenderRamen(){
+    let img=
+    img.image
+
+    let divCard=document.createElement("div")
+    divCard.append()
+
+}
+function updateRamen(){
+
+}
